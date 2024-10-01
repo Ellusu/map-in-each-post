45234a9523bc1b0bb1223392bd80e62d33a0a3cc
darwix <matteo.enna89@gmail.com>
2024-10-01 07:15:47 +0200

    admin js adjust

--- plugin/map-in-each-post.php
+++ plugin/map-in-each-post.php
@@ -2,7 +2,7 @@
    /*
    Plugin Name: Map in Each Post
    description: A simple plugin to insert customizable maps in posts using shortcodes. Supports unique maps per post and custom post types.
-   Version: 2.0.0
+   Version: 2.1
    Author: Matteo Enna
    Author URI: https://matteoenna.it/it/wordpress-work/
    Text Domain: map-in-each-post
